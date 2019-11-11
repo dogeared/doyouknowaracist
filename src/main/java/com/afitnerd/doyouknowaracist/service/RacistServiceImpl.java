4da5de50804f1586ae228c64ac9e9221566edd44
Micah Silverman <micah@afitnerd.com>
2019-11-10 22:38:54 -0500

    updated email list to compressed, base64 encoded

--- src/main/java/com/afitnerd/doyouknowaracist/service/RacistServiceImpl.java
+++ src/main/java/com/afitnerd/doyouknowaracist/service/RacistServiceImpl.java
@@ -6,26 +6,49 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
 
 @Service
 public class RacistServiceImpl implements RacistService {
 
 
     @Value("#{ @environment['racists'] }")
-    private List<String> racistsRaw;
+    private String racistsB64;
 
     private List<String> racists;
 
     @PostConstruct
-    void setup() {
-        racists = racistsRaw.stream()
-            .map(String::toLowerCase)
-            .collect(Collectors.toList());
+    void setup() throws IOException {
+        racists = Arrays.asList(parseRacists());
 
         // don't need it anymore
-        racistsRaw = null;
+        racistsB64 = null;
+    }
+
+    private String[] parseRacists() throws IOException  {
+        byte[] decodedBytes = Base64.getMimeDecoder().decode(racistsB64);
+        GZIPInputStream gzippedData = new GZIPInputStream(new ByteArrayInputStream(decodedBytes));
+        byte[] buffer = new byte[1024];
+        int len;
+        StringBuilder delimitedRacists = new StringBuilder();
+        try (Reader reader = new BufferedReader(new InputStreamReader(gzippedData, Charset.forName(StandardCharsets.UTF_8.name())))) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                delimitedRacists.append((char) c);
+            }
+        }
+        gzippedData.close();
+        return delimitedRacists.toString().split("\n");
     }
 
     @Override
